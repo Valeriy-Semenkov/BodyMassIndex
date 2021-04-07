b329c393bdf08fdc0dc2def6934410d44ffd3c0e
Valeriy Semenkov <unfairlly@gmail.com>
2021-04-07 22:56:06 +0300

    123

--- src/BmiService.java
+++ src/BmiService.java
@@ -2,7 +2,7 @@ public class BmiService {
     public double calculate(double bodymass, float height) {
         float a1 = height;
         float b1 = 2;
-        height = (float) Math.pow(a1, a2);
+        height = (float) Math.pow(a1, b1);
         int index = (int) (bodymass / height);
         return index;
 
