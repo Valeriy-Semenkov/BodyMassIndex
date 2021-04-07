f0d13ad5ae04436f2eb46c1acf33e3b6e4d46f9b
Valeriy-Semenkov <79516642+Valeriy-Semenkov@users.noreply.github.com>
2021-04-07 22:53:40 +0300

    Update BmiService.java

--- src/BmiService.java
+++ src/BmiService.java
@@ -2,7 +2,7 @@ public class BmiService {
     public double calculate(double bodymass, float height) {
         float a1 = height;
         float b1 = height;
-        height = (float) Math.pow(a1, b1);
+        height = (float) Math.pow(a1, 2);
         int index = (int) (bodymass / height);
         return index;
 
