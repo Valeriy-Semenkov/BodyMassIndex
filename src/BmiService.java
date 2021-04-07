6e8d18226ff53da675a63c9046656e389a8c315b
Valeriy Semenkov <unfairlly@gmail.com>
2021-04-07 17:09:31 +0300

    123

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,7 +1,7 @@
 public class BmiService {
     public double calculate(double bodymass, float height) {
-        float a1 =  height;
-        float b1 =  height;
+        float a1 = height;
+        float b1 = height;
         height = (float) Math.pow(a1, b1);
         int index = (int) (bodymass / height);
         return index;
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
         double bodymass = 89;
-        float height = (float) 1.85;{
+        float height = (float) 1.85;
+        {
 
             int index = (int) service.calculate(bodymass, height);
             System.out.println(index);
